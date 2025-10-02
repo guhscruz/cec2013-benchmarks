30e0a7b5f709a897b60ea3888009ff7ea3f1a85c
Gustavo <guhs.cruz@gmail.com>
2025-10-01 21:24:34 -0300

    Teste de funcionalidades

--- benchmarks.py
+++ benchmarks.py
@@ -1,4 +1,13 @@
-mean = 12
+from opfunu.cec_based import cec2017
+f3 = cec2017.F32017(ndim=30)
 
-if mean >= 7:
-    print("legal")
+from mealpy import GA, FloatVar
+
+problem = {
+    "obj_func": f3.evaluate,
+    "bounds": FloatVar(lb=f3.lb, ub=f3.ub),
+    "minmax": "min",
+}
+model = GA.BaseGA(epoch=100, pop_size=50)
+gbest = model.solve(problem)
+print(f"Solution: {gbest.solution}, Fit: {gbest.target.fitness}")
